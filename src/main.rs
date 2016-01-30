d94e880ece45f6836da255996bbe6fecf3f36694
Eli Dupree <vcs@elidupree.com>
2016-01-30 14:01:47 -0500

    Scrawl the initial melody

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,25 @@ extern crate hound;
 use codecophony::*;
 
 fn main() {
+write_eggs ();
+}
 
-  let manual = scrawl_MIDI_notes(
-                            "transpose 57 velocity 100 instrument 61
-12 and 15 and 19 5 8 step 0.5 5 8 10
-12 quiet sustain 17 quiet sustain 20 step \
-                             1 5 step 0.5 7 step 2.5
-finish release 17 release 20
+fn write_eggs () {
+  let main_melody = scrawl_MIDI_notes(
+                            "transpose 57 velocity 100 instrument 55
+        0 step 1 5 7 step 2 7 step 1 5 7 0 0 5 7 7 7 5 7 step 2
+        0 step 1 5 7 5 7 5 7 5 7 5 7 5 7 5 7 step 2
+          0 step 1 5 7 step 2 7 step 1 5 7 0 0 5 7 7 7 5 7 step 2
+          0 step 1 5 7 step 2 7 step 1 5 7 0 0 5 7 7 10 5 7 step 2 finish
 ");
-  let notes = Notes::combining(&[manual.clone(),
-                                 manual.translated(8.0),
-                                 manual.translated(16.0).transposed(7),
-                                 manual.translated(24.0).transposed(7)])
-                .scaled(0.25);
+let chorus_beat_part = scrawl_MIDI_notes ("velocity 100 percussion 35 step 2 35 and 38 finish");
+let mut chorus_beat = Notes::new ();
+for offset in 0..16 {chorus_beat.add (& chorus_beat_part.translated (offset as f64*4.0));}
+
+  let notes = Notes::combining(&[main_melody.clone (), chorus_beat.clone ()])
+                .scaled(30.0/144.0);
 
 
-  // add (0.0, 0); add (1.5, 5); add (2.0, 7); add (3.0, 11); add (4.0, 12);
 
   let music = notes.render_default(44100);
 
@@ -28,7 +31,7 @@ finish release 17 release 20
     sample_rate: 44100,
     bits_per_sample: 16,
   };
-  let mut writer = hound::WavWriter::create("output.wav", spec).unwrap();
+  let mut writer = hound::WavWriter::create("eggs.wav", spec).unwrap();
   for t in music.samples.iter() {
     writer.write_sample(*t as i16).unwrap();
 
