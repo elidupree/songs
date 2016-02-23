a9663a35b6717fdfff90adfbee6aae7727e3119a
Eli Dupree <vcs@elidupree.com>
2016-02-23 15:43:51 -0500

    Music development

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,16 @@ fn write_eggs () {
 ");
 for note in main_melody.iter_mut () {if note.basics.start % 4.0 == 2.0 {note.renderer.velocity = 127;}}
 let chorus_harmony = scrawl_MIDI_notes ("transpose 45 instrument 31 velocity 40
-at 2 0 and 4 and 7 and 12 at 18 4 and 12 and 19 at 34 0 and 4 and 7 and 12 at 50 2 and 7 and 17 and 22 at 64 finish ");
+at 2 0 and 4 and 7 and 12
+at 18 5 and 9 and 12 and 17
+at 34 0 and 4 and 7 and 12 
+at 50 2 and 7 and 17 and 22 
+at 64 finish ");
 let chorus_beat_part = scrawl_MIDI_notes ("velocity 100 percussion 35 step 2 35 and 38 finish");
 let mut chorus_beat = Notes::new ();
 for offset in 0..16 {chorus_beat.add (& chorus_beat_part.translated (offset as f64*4.0));}
+let mut prechorus_beat = Notes::new ();
+for offset in 0..8 {prechorus_beat.add (& scrawl_MIDI_notes ("velocity 100 percussion 35 step 4 38 finish"));}
 
 let segment_length = 64.0;
 
@@ -40,9 +46,14 @@ transpose 53 -2 0 0 2 2 4 4 5 5 strong 7 0 0 -2 0 advance 3");
 
 let standard_chorus_speed = 30.0/170.0;
 let standard_chorus_length = segment_length*standard_chorus_speed;
+let standard_prechorus_speed = 35.0/170.0;
+let standard_prechorus_length = segment_length*standard_prechorus_speed;
+let standard_verse_speed = 40.0/170.0;
+let standard_verse_length = segment_length*standard_verse_speed;
 
   let standard_chorus = Notes::combining(&[main_melody.clone (), chorus_harmony.clone (), chorus_beat.clone ()])
                 .scaled(standard_chorus_speed);
+let standard_prechorus = Notes::combining (& [main_melody.clone (), chorus_harmony.clone (), prechorus_beat.clone ()]).scaled (standard_prechorus_speed);
 
 let mut now = 0.0;
 let opening_chorus_start = now;
@@ -50,8 +61,11 @@ let mut notes = Notes::new ();
 notes.add (& standard_chorus.translated (opening_chorus_start));
 now += standard_chorus_length;
 let believe_start = now;
-notes.add (& believe_melody.translated (believe_start));
-
+notes.add (& believe_melody.translated (believe_start).scaled (standard_verse_speed));
+now += standard_verse_length;
+notes.add (& standard_prechorus.translated (now));
+now += standard_prechorus_length;
+notes.add (& standard_chorus.translated (now));
 
   let music = notes.render_default(44100);
 
